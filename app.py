4c3f0900616f3be0dfa78ea10a87d7240ef83dec
Peerapat <pplike212121@gmail.com>
2024-03-07 21:21:30 +0700

    set component

--- app.py
+++ app.py
@@ -3,23 +3,21 @@ window = tk.Tk()
 main_frame = tk.Frame(window)
 main_frame.pack(expand=True, pady=20)
 
+
 lbl_name = tk.Label(main_frame,text="°F(fahrenheit)")
 ent_name = tk.Entry(main_frame,text="text",fg="black",width=10)
-
-def handle_click():
-      F = float(ent_name.get())
-      run_Ans = (F-32)*5/9
-      Ans.config(text=f"{run_Ans:.2f}°C")
-
 btn_click = tk.Button(main_frame,text="=",command=handle_click)
 Ans = tk.Label(main_frame,text="100°C")
-
-
 
 ent_name.pack(side=tk.LEFT)
 lbl_name.pack(side=tk.LEFT)
 btn_click.pack(side=tk.LEFT)
 Ans.pack(side=tk.LEFT)
+
+def handle_click():
+      F = float(ent_name.get())
+      run_Ans = (F-32)*5/9
+      Ans.config(text=f"{run_Ans:.2f}°C")
 
 def handle_keypess(event):
       print(event.char)
